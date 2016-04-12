090c374f223dedce48036ffd5a9f8b45e467d1c6
Chris Nault <chrisnault@gmail.com>
2016-04-12 13:11:38 -0500

    add basic messaging via browser console using socket.io emit

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,12 @@
+// this is running in the browser so 'socket' is available in the browser console !!
+
 var socket = io();
 
 socket.on('connect', function () {
 	console.log('Connected to socket.io server!');
+});
+
+socket.on('message', function(message) {
+	console.log('New message');
+	console.log(message.text);
 });
--- server.js
+++ server.js
@@ -6,8 +6,19 @@ var io = require('socket.io')(http)
 
 app.use(express.static(__dirname + "/public"));
 
-io.on('connection', function() {
+io.on('connection', function(socket) {
 	console.log("User connected vi socket.io");
+	
+	socket.on('message', function (message) {
+		console.log('Message received: ' + message.text);
+		// io.emit sends to everone includeing the sender
+		// socket.broadcast.emit goes to everyone but the sender
+		socket.broadcast.emit('message', message);
+	});
+
+	socket.emit('message', {
+		text: 'Welcome to my chat app'
+	});
 });
 
 http.listen(PORT, function () {
