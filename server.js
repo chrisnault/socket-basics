4cb98aca4b848771035706635855dc451a110043
Chris Nault <chrisnault@gmail.com>
2016-04-14 13:13:52 -0500

    Make rooms private with socket.id

--- server.js
+++ server.js
@@ -7,16 +7,29 @@ var io = require('socket.io')(http)
 
 app.use(express.static(__dirname + "/public"));
 
+var clientInfo = {};
+
 io.on('connection', function(socket) {
 	console.log("User connected vi socket.io");
+	socket.on('joinRoom', function(req) {
+		clientInfo[socket.id] = req;
+		socket.join(req.room);
+		socket.broadcast.to(req.room).emit('message', {
+			name: 'System',
+			text: req.name + ' has joined!',
+			timestamp: moment.valueOf()
+		});
+	});
+
 	socket.on('message', function (message) {
 		console.log('Message received: ' + message.text);
 		// io.emit sends to everone includeing the sender
 		// socket.broadcast.emit goes to everyone but the sender
 		// socket.broadcast.emit('message', message);
 		message.timestamp = moment().valueOf();
-		io.emit('message', message);
+		io.to(clientInfo[socket.id].room).emit('message', message);
 	});
+
 	
 	socket.emit('message', {
 		text: 'Welcome to chat',
