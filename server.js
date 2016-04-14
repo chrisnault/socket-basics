8280ecbba591e17917d77adb5e8469fa8c73b881
Chris Nault <chrisnault@gmail.com>
2016-04-14 16:55:14 -0500

    add disconnect functions

--- server.js
+++ server.js
@@ -10,7 +10,23 @@ app.use(express.static(__dirname + "/public"));
 var clientInfo = {};
 
 io.on('connection', function(socket) {
-	console.log("User connected vi socket.io");
+	console.log("User connected via socket.io");
+
+	socket.on('disconnect', function() {
+		console.log('user is leaving');
+		var userData = clientInfo[socket.id];
+		if (typeof userData !== 'undefined') {
+			console.log(userData.name);
+			socket.leave(userData.room);
+			io.to(userData.room).emit('message', {
+				name: 'System',
+				text: userData.name + ' has left',
+				timestamp: moment.valueOf()
+			});
+			delete clientInfo[socket.id];
+		}
+	});
+
 	socket.on('joinRoom', function(req) {
 		clientInfo[socket.id] = req;
 		socket.join(req.room);
@@ -27,7 +43,7 @@ io.on('connection', function(socket) {
 		// socket.broadcast.emit goes to everyone but the sender
 		// socket.broadcast.emit('message', message);
 		message.timestamp = moment().valueOf();
-		io.to(clientInfo[socket.id].room).emit('message', message);
+		io.to(clientInfo[socket.id].room).emit('message', message); //HERE
 	});
 
 	
